ceaf6b7f8ac1cdceedd82008f7109f11be15059b
michakrzeminski <wiorek455@wp.pl>
2016-12-14 14:03:03 +0100

    Client GUI

--- ManagementApp/ClientNode/Program.cs
+++ ManagementApp/ClientNode/Program.cs
@@ -20,13 +20,12 @@ namespace ClientNode
             //TODO jaki typ mam wysyłac a jeśli vc3 to w którą pozycje 0, 1 czy 2
             //virtual ip, listener in/out, port do managmentu
             //string[] parameters = new string[] { "165.23.12.32", "10002", "10001"};
-            client = new ClientNode(parameters);
+            //client = new ClientNode(parameters);
             //GUI
-            //Application.EnableVisualStyles();
-            //Application.SetCompatibleTextRenderingDefault(false);
-
-            //ClientWindow window = new ClientWindow(parameters);
-            //Application.Run(window);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            ClientWindow window = new ClientWindow(parameters);
+            Application.Run(window);
         }
     }
 }
