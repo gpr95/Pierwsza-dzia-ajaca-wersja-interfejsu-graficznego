cd39dccfbb91db080d7c98a8fdde36253d681b96
Mark12012 <Mark12012@gmail.com>
2017-01-14 17:54:39 +0100

    Update bat-a.

--- ManagementApp/Management/Program.cs
+++ ManagementApp/Management/Program.cs
@@ -10,8 +10,8 @@ namespace Management
     {
         static void Main(string[] args)
         {
-            string[] parameters = new string[] { args[0], args[1] };
-
+            //string[] parameters = new string[] { args[0], args[1] };
+            string[] parameters = new string[] { "7777", "7778" };
             ManagementPlane management = new ManagementPlane(parameters);
         }
     }
--- ManagementApp/copyExecs.bat
+++ ManagementApp/copyExecs.bat
@@ -1,3 +1,4 @@
 xcopy CableCloud\bin\Debug\CableCloud.exe ManagementApp\bin\Debug\ /D /Y
 xcopy ClientNode\bin\Debug\ClientNode.exe ManagementApp\bin\Debug\ /D /Y
 xcopy NetNode\bin\Debug\NetNode.exe ManagementApp\bin\Debug\ /D /Y
+xcopy Management\bin\Debug\Management.exe ManagementApp\bin\Debug\ /D /Y
