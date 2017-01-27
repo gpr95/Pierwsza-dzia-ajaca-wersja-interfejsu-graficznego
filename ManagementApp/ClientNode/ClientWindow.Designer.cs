276317c0e91419bf20ec0bfe8fffcd19d0c9e25f
jamesbonk <patryk.bak@gazeta.pl>
2017-01-27 02:14:54 +0100

    start gui rebuild

--- ManagementApp/ClientNode/ClientWindow.Designer.cs
+++ ManagementApp/ClientNode/ClientWindow.Designer.cs
@@ -34,12 +34,9 @@
             this.receivedTextBox = new System.Windows.Forms.TextBox();
             this.receivedLbl = new System.Windows.Forms.Label();
             this.sendingGroupBox = new System.Windows.Forms.GroupBox();
-            this.sendParamsGroupBox1 = new System.Windows.Forms.GroupBox();
-            this.addButton = new System.Windows.Forms.Button();
-            this.slotTextBox = new System.Windows.Forms.TextBox();
-            this.nodeTextBox = new System.Windows.Forms.TextBox();
-            this.slotLbl = new System.Windows.Forms.Label();
-            this.nodeLbl = new System.Windows.Forms.Label();
+            this.connectButton = new System.Windows.Forms.Button();
+            this.speedLbl = new System.Windows.Forms.Label();
+            this.speedComboBox = new System.Windows.Forms.ComboBox();
             this.stopSendingBtn = new System.Windows.Forms.Button();
             this.sendingTextBox = new System.Windows.Forms.TextBox();
             this.sendComboBox = new System.Windows.Forms.ComboBox();
@@ -47,8 +44,14 @@
             this.timeLbl = new System.Windows.Forms.Label();
             this.sendBtn = new System.Windows.Forms.Button();
             this.timeTextBox = new System.Windows.Forms.TextBox();
-            this.portLbl = new System.Windows.Forms.Label();
+            this.sendParamsGroupBox1 = new System.Windows.Forms.GroupBox();
             this.portTextBox = new System.Windows.Forms.TextBox();
+            this.portLbl = new System.Windows.Forms.Label();
+            this.addButton = new System.Windows.Forms.Button();
+            this.slotTextBox = new System.Windows.Forms.TextBox();
+            this.nodeTextBox = new System.Windows.Forms.TextBox();
+            this.slotLbl = new System.Windows.Forms.Label();
+            this.nodeLbl = new System.Windows.Forms.Label();
             this.logPanel.SuspendLayout();
             this.sendingGroupBox.SuspendLayout();
             this.sendParamsGroupBox1.SuspendLayout();
@@ -64,7 +67,7 @@
             this.logPanel.ForeColor = System.Drawing.Color.Red;
             this.logPanel.Location = new System.Drawing.Point(582, 13);
             this.logPanel.Name = "logPanel";
-            this.logPanel.Size = new System.Drawing.Size(335, 219);
+            this.logPanel.Size = new System.Drawing.Size(335, 279);
             this.logPanel.TabIndex = 1;
             // 
             // logTextBox
@@ -72,12 +75,12 @@
             this.logTextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(55)))));
             this.logTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.logTextBox.ForeColor = System.Drawing.Color.White;
-            this.logTextBox.Location = new System.Drawing.Point(20, 124);
+            this.logTextBox.Location = new System.Drawing.Point(20, 151);
             this.logTextBox.Multiline = true;
             this.logTextBox.Name = "logTextBox";
             this.logTextBox.ReadOnly = true;
             this.logTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.logTextBox.Size = new System.Drawing.Size(301, 84);
+            this.logTextBox.Size = new System.Drawing.Size(301, 117);
             this.logTextBox.TabIndex = 3;
             // 
             // logLbl
@@ -85,7 +88,7 @@
             this.logLbl.AutoSize = true;
             this.logLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
             this.logLbl.ForeColor = System.Drawing.Color.DarkOrange;
-            this.logLbl.Location = new System.Drawing.Point(13, 101);
+            this.logLbl.Location = new System.Drawing.Point(13, 131);
             this.logLbl.Name = "logLbl";
             this.logLbl.Size = new System.Drawing.Size(32, 17);
             this.logLbl.TabIndex = 2;
@@ -101,7 +104,7 @@
             this.receivedTextBox.Name = "receivedTextBox";
             this.receivedTextBox.ReadOnly = true;
             this.receivedTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.receivedTextBox.Size = new System.Drawing.Size(301, 61);
+            this.receivedTextBox.Size = new System.Drawing.Size(301, 88);
             this.receivedTextBox.TabIndex = 1;
             // 
             // receivedLbl
@@ -118,7 +121,9 @@
             // sendingGroupBox
             // 
             this.sendingGroupBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.sendingGroupBox.Controls.Add(this.sendParamsGroupBox1);
+            this.sendingGroupBox.Controls.Add(this.connectButton);
+            this.sendingGroupBox.Controls.Add(this.speedLbl);
+            this.sendingGroupBox.Controls.Add(this.speedComboBox);
             this.sendingGroupBox.Controls.Add(this.stopSendingBtn);
             this.sendingGroupBox.Controls.Add(this.sendingTextBox);
             this.sendingGroupBox.Controls.Add(this.sendComboBox);
@@ -136,78 +141,44 @@
             this.sendingGroupBox.TabStop = false;
             this.sendingGroupBox.Text = "SENDING";
             // 
-            // sendParamsGroupBox1
+            // connectButton
             // 
-            this.sendParamsGroupBox1.Controls.Add(this.portTextBox);
-            this.sendParamsGroupBox1.Controls.Add(this.portLbl);
-            this.sendParamsGroupBox1.Controls.Add(this.addButton);
-            this.sendParamsGroupBox1.Controls.Add(this.slotTextBox);
-            this.sendParamsGroupBox1.Controls.Add(this.nodeTextBox);
-            this.sendParamsGroupBox1.Controls.Add(this.slotLbl);
-            this.sendParamsGroupBox1.Controls.Add(this.nodeLbl);
-            this.sendParamsGroupBox1.ForeColor = System.Drawing.Color.DarkOrange;
-            this.sendParamsGroupBox1.Location = new System.Drawing.Point(173, 124);
-            this.sendParamsGroupBox1.Name = "sendParamsGroupBox1";
-            this.sendParamsGroupBox1.Size = new System.Drawing.Size(359, 54);
-            this.sendParamsGroupBox1.TabIndex = 7;
-            this.sendParamsGroupBox1.TabStop = false;
-            this.sendParamsGroupBox1.Text = "SENDING PARAMETERS";
+            this.connectButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(55)))));
+            this.connectButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.connectButton.Location = new System.Drawing.Point(407, 125);
+            this.connectButton.Name = "connectButton";
+            this.connectButton.Size = new System.Drawing.Size(125, 23);
+            this.connectButton.TabIndex = 9;
+            this.connectButton.Text = "Connect";
+            this.connectButton.UseVisualStyleBackColor = false;
+            this.connectButton.Click += new System.EventHandler(this.connectButton_Click);
             // 
-            // addButton
+            // speedLbl
             // 
-            this.addButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
-            this.addButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.addButton.Location = new System.Drawing.Point(265, 17);
-            this.addButton.Name = "addButton";
-            this.addButton.Size = new System.Drawing.Size(73, 23);
-            this.addButton.TabIndex = 4;
-            this.addButton.Text = "Add";
-            this.addButton.UseVisualStyleBackColor = false;
-            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            this.speedLbl.AutoSize = true;
+            this.speedLbl.Location = new System.Drawing.Point(170, 130);
+            this.speedLbl.Name = "speedLbl";
+            this.speedLbl.Size = new System.Drawing.Size(41, 13);
+            this.speedLbl.TabIndex = 8;
+            this.speedLbl.Text = "Speed:";
             // 
-            // slotTextBox
+            // speedComboBox
             // 
-            this.slotTextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
-            this.slotTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.slotTextBox.ForeColor = System.Drawing.SystemColors.Window;
-            this.slotTextBox.Location = new System.Drawing.Point(129, 18);
-            this.slotTextBox.Name = "slotTextBox";
-            this.slotTextBox.Size = new System.Drawing.Size(28, 20);
-            this.slotTextBox.TabIndex = 3;
-            // 
-            // nodeTextBox
-            // 
-            this.nodeTextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
-            this.nodeTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.nodeTextBox.ForeColor = System.Drawing.SystemColors.Window;
-            this.nodeTextBox.Location = new System.Drawing.Point(49, 17);
-            this.nodeTextBox.Name = "nodeTextBox";
-            this.nodeTextBox.Size = new System.Drawing.Size(40, 20);
-            this.nodeTextBox.TabIndex = 2;
-            // 
-            // slotLbl
-            // 
-            this.slotLbl.AutoSize = true;
-            this.slotLbl.Location = new System.Drawing.Point(95, 22);
-            this.slotLbl.Name = "slotLbl";
-            this.slotLbl.Size = new System.Drawing.Size(28, 13);
-            this.slotLbl.TabIndex = 1;
-            this.slotLbl.Text = "Slot:";
-            // 
-            // nodeLbl
-            // 
-            this.nodeLbl.AutoSize = true;
-            this.nodeLbl.Location = new System.Drawing.Point(7, 20);
-            this.nodeLbl.Name = "nodeLbl";
-            this.nodeLbl.Size = new System.Drawing.Size(36, 13);
-            this.nodeLbl.TabIndex = 0;
-            this.nodeLbl.Text = "Node:";
+            this.speedComboBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
+            this.speedComboBox.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.speedComboBox.ForeColor = System.Drawing.SystemColors.Window;
+            this.speedComboBox.FormattingEnabled = true;
+            this.speedComboBox.Location = new System.Drawing.Point(254, 127);
+            this.speedComboBox.Name = "speedComboBox";
+            this.speedComboBox.Size = new System.Drawing.Size(121, 21);
+            this.speedComboBox.TabIndex = 7;
+            this.speedComboBox.SelectedIndexChanged += new System.EventHandler(this.speedComboBox_SelectedIndexChanged);
             // 
             // stopSendingBtn
             // 
             this.stopSendingBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(55)))));
             this.stopSendingBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.stopSendingBtn.Location = new System.Drawing.Point(367, 184);
+            this.stopSendingBtn.Location = new System.Drawing.Point(407, 183);
             this.stopSendingBtn.Name = "stopSendingBtn";
             this.stopSendingBtn.Size = new System.Drawing.Size(125, 24);
             this.stopSendingBtn.TabIndex = 6;
@@ -284,37 +255,104 @@
             this.timeTextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(55)))));
             this.timeTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.timeTextBox.ForeColor = System.Drawing.Color.White;
-            this.timeTextBox.Location = new System.Drawing.Point(238, 187);
+            this.timeTextBox.Location = new System.Drawing.Point(254, 187);
             this.timeTextBox.Name = "timeTextBox";
-            this.timeTextBox.Size = new System.Drawing.Size(92, 20);
+            this.timeTextBox.Size = new System.Drawing.Size(121, 20);
             this.timeTextBox.TabIndex = 4;
             this.timeTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
-            // portLbl
+            // sendParamsGroupBox1
             // 
-            this.portLbl.AutoSize = true;
-            this.portLbl.Location = new System.Drawing.Point(164, 20);
-            this.portLbl.Name = "portLbl";
-            this.portLbl.Size = new System.Drawing.Size(29, 13);
-            this.portLbl.TabIndex = 5;
-            this.portLbl.Text = "Port:";
+            this.sendParamsGroupBox1.Controls.Add(this.portTextBox);
+            this.sendParamsGroupBox1.Controls.Add(this.portLbl);
+            this.sendParamsGroupBox1.Controls.Add(this.addButton);
+            this.sendParamsGroupBox1.Controls.Add(this.slotTextBox);
+            this.sendParamsGroupBox1.Controls.Add(this.nodeTextBox);
+            this.sendParamsGroupBox1.Controls.Add(this.slotLbl);
+            this.sendParamsGroupBox1.Controls.Add(this.nodeLbl);
+            this.sendParamsGroupBox1.ForeColor = System.Drawing.Color.DarkOrange;
+            this.sendParamsGroupBox1.Location = new System.Drawing.Point(17, 238);
+            this.sendParamsGroupBox1.Name = "sendParamsGroupBox1";
+            this.sendParamsGroupBox1.Size = new System.Drawing.Size(559, 54);
+            this.sendParamsGroupBox1.TabIndex = 8;
+            this.sendParamsGroupBox1.TabStop = false;
+            this.sendParamsGroupBox1.Text = "HARD PERNAMENT PARAMETERS";
             // 
             // portTextBox
             // 
             this.portTextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
             this.portTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.portTextBox.ForeColor = System.Drawing.SystemColors.Window;
-            this.portTextBox.Location = new System.Drawing.Point(199, 17);
+            this.portTextBox.Location = new System.Drawing.Point(300, 17);
             this.portTextBox.Name = "portTextBox";
-            this.portTextBox.Size = new System.Drawing.Size(45, 20);
+            this.portTextBox.Size = new System.Drawing.Size(75, 20);
             this.portTextBox.TabIndex = 6;
+            // 
+            // portLbl
+            // 
+            this.portLbl.AutoSize = true;
+            this.portLbl.Location = new System.Drawing.Point(265, 20);
+            this.portLbl.Name = "portLbl";
+            this.portLbl.Size = new System.Drawing.Size(29, 13);
+            this.portLbl.TabIndex = 5;
+            this.portLbl.Text = "Port:";
+            // 
+            // addButton
+            // 
+            this.addButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
+            this.addButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.addButton.Location = new System.Drawing.Point(407, 18);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(125, 23);
+            this.addButton.TabIndex = 4;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = false;
+            // 
+            // slotTextBox
+            // 
+            this.slotTextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
+            this.slotTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.slotTextBox.ForeColor = System.Drawing.SystemColors.Window;
+            this.slotTextBox.Location = new System.Drawing.Point(173, 18);
+            this.slotTextBox.Name = "slotTextBox";
+            this.slotTextBox.Size = new System.Drawing.Size(75, 20);
+            this.slotTextBox.TabIndex = 3;
+            // 
+            // nodeTextBox
+            // 
+            this.nodeTextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
+            this.nodeTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.nodeTextBox.ForeColor = System.Drawing.SystemColors.Window;
+            this.nodeTextBox.Location = new System.Drawing.Point(49, 17);
+            this.nodeTextBox.Name = "nodeTextBox";
+            this.nodeTextBox.Size = new System.Drawing.Size(75, 20);
+            this.nodeTextBox.TabIndex = 2;
+            // 
+            // slotLbl
+            // 
+            this.slotLbl.AutoSize = true;
+            this.slotLbl.Location = new System.Drawing.Point(139, 20);
+            this.slotLbl.Name = "slotLbl";
+            this.slotLbl.Size = new System.Drawing.Size(28, 13);
+            this.slotLbl.TabIndex = 1;
+            this.slotLbl.Text = "Slot:";
+            // 
+            // nodeLbl
+            // 
+            this.nodeLbl.AutoSize = true;
+            this.nodeLbl.Location = new System.Drawing.Point(7, 20);
+            this.nodeLbl.Name = "nodeLbl";
+            this.nodeLbl.Size = new System.Drawing.Size(36, 13);
+            this.nodeLbl.TabIndex = 0;
+            this.nodeLbl.Text = "Node:";
             // 
             // ClientWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.ClientSize = new System.Drawing.Size(934, 242);
+            this.ClientSize = new System.Drawing.Size(934, 333);
+            this.Controls.Add(this.sendParamsGroupBox1);
             this.Controls.Add(this.sendingGroupBox);
             this.Controls.Add(this.logPanel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
@@ -346,13 +384,16 @@
         private System.Windows.Forms.Button sendBtn;
         private System.Windows.Forms.TextBox timeTextBox;
         private System.Windows.Forms.Button stopSendingBtn;
+        private System.Windows.Forms.Button connectButton;
+        private System.Windows.Forms.Label speedLbl;
+        private System.Windows.Forms.ComboBox speedComboBox;
         private System.Windows.Forms.GroupBox sendParamsGroupBox1;
+        private System.Windows.Forms.TextBox portTextBox;
+        private System.Windows.Forms.Label portLbl;
         private System.Windows.Forms.Button addButton;
         private System.Windows.Forms.TextBox slotTextBox;
         private System.Windows.Forms.TextBox nodeTextBox;
         private System.Windows.Forms.Label slotLbl;
         private System.Windows.Forms.Label nodeLbl;
-        private System.Windows.Forms.TextBox portTextBox;
-        private System.Windows.Forms.Label portLbl;
     }
 }
